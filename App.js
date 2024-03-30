22237be95cc3bf1c4d3ff2640126b7e492c944f6
unknown <sagarshekhar700@gmail.com>
2024-03-31 00:13:40 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+{/* <div id="parent">
+<div class="child1">
+    <h1></h1>
+    <h2></h2>
+</div>
+<div class="child1">
+    <h1></h1>
+    <h2></h2>
+</div>
+</div> */}
+
+const parent = React.createElement("div", { id: 'parent' },
+    [React.createElement("div", { class: "child1" },
+        [React.createElement("h1", {}, "H1 Tag"),
+        React.createElement("h2", {}, "H2 Tag")]),
+    React.createElement("div", { class: "child2" },
+        [React.createElement("h1", {}, "H1 Tag"),
+        React.createElement("h2", {}, "H2 Tag")]
+    )]);
+
+
+
+const heading =
+    React.createElement("h1",
+        { id: "heading", class: "head" },
+        "Hello World From React"
+    );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
